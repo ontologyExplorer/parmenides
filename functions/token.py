dea2c37fcef445b46108746b76f2bb200bacf9ba
Steven Fletcher <fletcher.steven@hotmail.fr>
2024-03-01 14:57:11 +0100

    added module docstring

--- functions/token.py
+++ functions/token.py
@@ -1,3 +1,35 @@
+"""
+This module provides the AccessTokenRetriever class for retrieving access tokens from an API.
+
+The AccessTokenRetriever class is designed to authenticate and retrieve access tokens 
+the https://smt.esante.gouv.fr API. It requires the user to have an account with
+the specified site and uses the user's credentials to obtain tokens necessary for API access.
+
+The class supports loading configuration from a specified JSON file, which should contain
+the endpoint URL for token retrieval, client identification details, and the necessary
+authentication credentials. It provides a method to send a POST request to the API with these
+credentials to retrieve an access token.
+
+Dependencies:
+    - json: For parsing and loading the configuration file.
+    - requests: For making HTTP requests to the API.
+
+Usage:
+    - Initialize the AccessTokenRetriever with a path to a configuration file.
+    - Call get_access_token with the user's email and password to retrieve an access token.
+
+Example configuration file structure:
+{
+    "query_token_url": "URL for token endpoint",
+    "client_id": "Client ID for authentication",
+    "refresh_token": "Refresh token for authentication",
+    "content_type": "Content type for HTTP headers"
+}
+
+This module simplifies the process of authenticating with the https://smt.esante.gouv.fr API
+and abstracts the details of making HTTP requests and handling responses.
+"""
+
 import json
 
 import requests
@@ -7,7 +39,8 @@ class AccessTokenRetriever:
     """
     Retrieving the Token Access for the API.
     Before using this application be sure to create an account at https://smt.esante.gouv.fr.
-    The method get_access_token will send a POST request to the API using the specific https://smt.esante.gouv.fr user's credentials.
+    The method get_access_token will send a POST request to the API
+    using the specific https://smt.esante.gouv.fr user's credentials.
 
     Attributes:
         query_token_url (str): The URL of the token endpoint.
@@ -42,12 +75,13 @@ class AccessTokenRetriever:
         Returns:
             dict: Configuratino parameters
         """
-        with open(config_path, "r") as file:
+        with open(config_path, "r", encoding="utf-8") as file:
             return json.load(file)
 
     def get_access_token(self, mail: str, password: str) -> str | None:
         """
-        Send a POST request to the API using the specific https://smt.esante.gouv.fr user's credentials
+        Send a POST request to the API using the specific
+        https://smt.esante.gouv.fr user's credentials
 
         Args:
             mail (str): User's mail
